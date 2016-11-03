05741e94a83119eb719991d68d73b3d384a09784
hankvenom <hankvenom@gmail.com>
2016-11-03 01:22:50 +0100

    First Commit
    Introducing dimensions logic.

---
+++ GameOff2016/Assets/LayerMaskInheritance.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerMaskInheritance : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Transform[] ts = gameObject.GetComponentsInChildren<Transform>();
+
+        foreach (var child in ts)
+            child.gameObject.layer=gameObject.layer;
+	}
+	
+}
